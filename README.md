b325c4a6894566aced0b2c02ae39d1fc74a9e417
Stephen <stephen@beechens.com>
2021-03-14 16:19:35 -0600

    Initial staging repo config

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
-# hgdb-dev-staging
- 
+# HAGDB Development Staging Branch
+
+## About
+
+Thsi is a staging development branch for Home Assistant Google Drive Backup.  If you are a normal user trying to backup your Home Assistant 
+snapshots to Google Drive please leave here and install the addon at [this repository](https://github.com/sabeechen/hassio-google-drive-backup).
+This repository is intended for use by developers, and as such is likely to contain unverified code.  The dev branch is pushed here on every checkin, and as such it often has code that might not be ready for a wide release.  This can mean that:
+
+- It might stop working randomly when a new version is pushed automatically
+- It might delete all of your snapshots.
+- It might corrupt your Home Assistant installation.
---
+++ update.py
@@ -0,0 +1,36 @@
+import json
+import sys
+import os
+from os.path import join
+
+
+async def main():
+    
+    source_repo = sys.argv[0]
+    staging_repo = sys.argv[1]
+    run_number = os.environ.get('GITHUB_RUN_NUMBER', -1)
+    with open(join(source_repo, "hassio-google-drive-backup", "config.json")) as f:
+        source_config = json.load(f)
+        print("Source verison is '{0}'".format(source_config['version']))
+
+    with open(join(staging_repo, "hassio-google-drive-backup", "config.json")) as f:
+        staging_config = json.load(f)
+        print("Current staging version is '{0}'".format(staging_config['version']))
+
+    new_version = source_config['version'] + ".staging." + run_number
+
+    print("New staging version will be '{0}'".format(new_version))
+    source_config['version'] = new_version
+    source_config['name'] = source_config['name'] + " (staging)"
+    source_config['slug'] = source_config['slug'] + "_staging"
+    source_config['image'] = source_config['image'] + "_staging"
+    source_config['panel_title'] = source_config['panel_title'] + "(staging)"
+    with open(join(source_repo, "hassio-google-drive-backup", "config.json"), "w") as f:
+        json.dump(source_config, f)
+    with open(join(staging_repo, "hassio-google-drive-backup", "config.json"), "w") as f:
+        json.dump(source_config, f)
+
+
+if __name__ == '__main__':
+    print("Starting")
+    main()
